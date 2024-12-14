64a712ff5ed2934a2cfc1fbf1f89090093ed78bd
JakobKleeberger <kleeberger.jakob@aol.com>
2024-12-14 18:28:07 +0100

    added sonarr and radarr

--- modules/nixos/hosting/jellyfin.nix
+++ modules/nixos/hosting/jellyfin.nix
@@ -4,6 +4,19 @@
     openFirewall = true;
   };
 
+  services.radarr = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+    user = "homelab";
+    group = "users";
+    dataDir = "/home/homelab/movies";
+  };
+
   environment.systemPackages = with pkgs; [
     jellyfin
     jellyfin-web
