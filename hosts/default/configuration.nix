62aedebc25b3ba9d834cf28a5083ab3bb032b07e
JakobKleeberger <kleeberger.jakob@aol.com>
2025-05-08 20:49:15 +0200

    feat: added greetd + added thunderbolt 4 support + refactor

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -1,19 +1,13 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ config
-, pkgs
-, nixpkgs
-, inputs
-, ...
-}: {
+{ config, pkgs, nixpkgs, inputs, ... }: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
 
     inputs.home-manager.nixosModules.default
 
-    # ../../modules/home-manager/terminal
     ../../modules/nixos/util/stylix/stylix-nixos.nix
     ../../modules/nixos/util/keyd
   ];
@@ -80,7 +74,8 @@
       after = [ "graphical-session.target" ];
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
         Restart = "on-failure";
         RestartSec = 1;
         TimeoutStopSec = 10;
@@ -88,13 +83,19 @@
     };
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd hyprland";
+        user = "jakob";
+      };
+      default_session = initial_session;
+    };
+  };
 
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
+  services.xserver.synaptics.enable = true;
+  services.fwupd.enable = true;
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "de";
@@ -108,6 +109,9 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
+
+  # Enable Thunderbolt
+  services.hardware.bolt.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -124,9 +128,6 @@
     isNormalUser = true;
     description = "jakob";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    ];
   };
 
   programs.hyprland.enable = true;
@@ -149,9 +150,7 @@
 
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
-    users = {
-      "jakob" = import ./home.nix;
-    };
+    users = { "jakob" = import ./home.nix; };
   };
 
   # Allow unfree packages
@@ -160,66 +159,57 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     # Hyprland dependencies
-    pkgs.waybar
-    pkgs.hyprlock
-    pkgs.hyprpaper
-    pkgs.hyprshot
-    pkgs.wofi
-    # Bluetooth Manager
+    waybar
+    hyprlock
+    hyprpaper
+    hyprshot
+    wofi
     # Python3
-    pkgs.python312Full
-    pkgs.poetry
+    python312Full
+    poetry
     # Audio
-    pkgs.pavucontrol
+    pavucontrol
     #  Man Pages
-    pkgs.pkgs.man-pages
-    pkgs.man-pages-posix
+    man-pages
+    man-pages-posix
 
     # Versioning
-    pkgs.pkgs.git
-    pkgs.gitui
+    git
+    gitui
 
     # Tools
-    pkgs.unzip
-    pkgs.age
-    pkgs.fastfetch
-    pkgs.wget
-    pkgs.dua
-    pkgs.coreutils
-    pkgs.fd
+    unzip
+    age
+    fastfetch
+    wget
+    dua
+    ripgrep
+    coreutils
+    fd
 
     # Polkit
-    pkgs.polkit_gnome
-    pkgs.kdePackages.polkit-kde-agent-1
+    polkit_gnome
+    kdePackages.polkit-kde-agent-1
+
+    fwupd
 
     # Tailscale
-    pkgs.tailscale
+    tailscale
 
     # Apps
-    pkgs.ghostty
-    pkgs.ausweisapp
-    pkgs.signal-desktop
-    pkgs.telegram-desktop
-    pkgs.tor-browser-bundle-bin
-    pkgs.discord
-    pkgs.ciscoPacketTracer8
-    pkgs.emacs-nox
+    ghostty
+    ausweisapp
+    signal-desktop
+    telegram-desktop
+    tor-browser-bundle-bin
+    discord
+    ciscoPacketTracer8
   ];
 
   # Activate Nix Flakes and nix-command
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
