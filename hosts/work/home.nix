f00c0b0a5a8187e079446960b30055794c543384
JakobKleeberger <kleeberger.jakob@aol.com>
2026-01-24 17:09:30 +0100

    REFACTOR: work added noctalia and moved emacs config

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -9,6 +9,7 @@
     # ../../modules/home-manager/terminal
     ../../modules/nixos/util/stylix/stylix-home.nix
     ../../modules/home-manager/terminal
+    ../../modules/home-manager/utils
   ];
 
   home.username = "jakob";
@@ -42,43 +43,12 @@
   home.file = {
     ".config/hypr".source = ../../dotfiles/hypr;
     ".config/waybar".source = ../../dotfiles/waybar;
-    "Pictures/wallpapers".source = ../../modules/nixos/util/stylix/wallpapers;
+    # "Pictures/wallpapers".source = ../../modules/nixos/util/stylix/wallpapers;
     "~/".source = ../../dotfiles/idea;
   };
 
   home.sessionVariables = {
     EDITOR = "nvim";
-  };
-
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs-pgtk;
-    # package = pkgs.emacs-nox;
-    extraPackages = epkgs: [
-      epkgs.treesit-grammars.with-all-grammars
-
-      pkgs.jetbrains-mono
-      pkgs.nerd-fonts.jetbrains-mono
-      pkgs.nerd-fonts.symbols-only
-
-      # Rust
-      pkgs.rust-analyzer
-      pkgs.rustfmt
-      pkgs.cargo
-      pkgs.gcc_multi
-      # Python
-      pkgs.pyright
-      pkgs.ruff
-      # Nix
-      pkgs.nil
-      pkgs.nixd
-      pkgs.nixdoc
-      pkgs.nixfmt-classic
-      # Latex
-      pkgs.gnuplot
-      # Java
-      pkgs.jdt-language-server
-    ];
   };
 
   programs.bash.enable = true;
