8f4548d0121a0cac059caa202faa6546c6f11b69
JakobKleeberger <kleeberger.jakob@aol.com>
2026-01-24 15:51:23 +0100

    REFACTOR: moved emacs from home to utils

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -8,13 +8,12 @@
     ../../modules/nixos/utils/stylix/stylix-home.nix
     ../../modules/home-manager/terminal
     ../../modules/home-manager/utils
-    # inputs.noctalia.homeModules.default
   ];
 
   home.username = "jakob";
   home.homeDirectory = "/home/jakob";
 
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.stateVersion = "unstable"; # Please read the comment before changing.
 
   fonts.fontconfig.enable = true;
 
@@ -38,17 +37,7 @@
   home.file = {
     ".config/hypr".source = ../../dotfiles/hypr;
     ".config/waybar".source = ../../dotfiles/waybar;
-    # ".config/emacs".source = ../../dotfiles/emacs;
-    # "Pictures/Wallpapers".source = ../../modules/nixos/util/stylix/wallpapers;
     "~/".source = ../../dotfiles/idea;
-    # ".cache/noctalia/wallpapers.json" = {
-    #   text = builtins.toJSON {
-    #     defaultWallpaper = ../../modules/nixos/util/stylix/wallpapers/mountains.jpg;
-    #     wallpapers = {
-    #       "DP-1" = ../../modules/nixos/util/stylix/wallpapers/mountains.jpg;
-    #     };
-    #   };
-    # };
   };
 
   home.sessionVariables = {
@@ -57,114 +46,10 @@
 
   programs.git = {
     enable = true;
-    userName = "JakobKleeberger";
-    userEmail = "kleeberger.jakob@aol.com";
+    settings.user.name = "JakobKleeberger";
+    settings.user.email = "kleeberger.jakob@aol.com";
     lfs.enable = true;
   };
-
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs-pgtk;
-    extraPackages = epkgs: [
-      epkgs.treesit-grammars.with-all-grammars
-
-      pkgs.jetbrains-mono
-      pkgs.nerd-fonts.jetbrains-mono
-      pkgs.nerd-fonts.symbols-only
-
-      # Rust
-      pkgs.rust-analyzer
-      pkgs.rustfmt
-      pkgs.cargo
-      pkgs.gcc_multi
-      # Python
-      pkgs.pyright
-      pkgs.ruff
-      # Nix
-      pkgs.nil
-      pkgs.nixd
-      pkgs.nixdoc
-      pkgs.nixfmt-classic
-      # Latex
-      pkgs.gnuplot
-      # Java
-      pkgs.jdt-language-server
-    ];
-  };
-
-  # configure options
-  # programs.noctalia-shell = {
-  #   enable = true;
-  #   settings = {
-  #     # configure noctalia here
-  #     bar = {
-  #       density = "compact";
-  #       position = "top";
-  #       floating = true;
-  #       showCapsule = false;
-  #       widgets = {
-  #         left = [
-  #           {
-  #             id = "ControlCenter";
-  #             useDistroLogo = true;
-  #           }
-  #           {
-  #             hideUnoccupied = false;
-  #             id = "Workspace";
-  #             labelMode = "index";
-  #           }
-  #         ];
-  #         center = [ ];
-  #         right = [
-  #           {
-  #             id = "Network";
-  #           }
-  #           {
-  #             id = "Bluetooth";
-  #           }
-  #           {
-  #             alwaysShowPercentage = true;
-  #             id = "Battery";
-  #             warningThreshold = 30;
-  #           }
-  #           {
-  #             formatHorizontal = "HH:mm";
-  #             formatVertical = "HH mm";
-  #             id = "Clock";
-  #             useMonospacedFont = true;
-  #             usePrimaryColor = true;
-  #           }
-  #         ];
-  #       };
-  #     };
-  #     colorSchemes.predefinedScheme = "Monochrome";
-  #     general = {
-  #       avatarImage = "/home/drfoobar/.face";
-  #       radiusRatio = 0.2;
-  #     };
-  #     appLauncher = {
-  #       terminalCommand = "ghostty -e";
-  #       viewMode = "grid";
-  #       showCategories = false;
-  #     };
-  #     location = {
-  #       name = "Bonn";
-  #       weatherEnabled = true;
-  #       weatherShowEffects = true;
-  #       showWeekNumberInCalendar = true;
-  #       showCalendarEvents = true;
-  #       showCalendarWeather = false;
-  #       monthBeforeDay = true;
-  #       firstDayOfTheWeek = 0;
-  #     };
-  #     wallpaper = {
-  #       enabled = true;
-  #       directory = ../../modules/nixos/util/stylix/wallpapers;
-  #       setWallpapersOnAllMonitors = true;
-  #     };
-  #   };
-  #   # this may also be a string or a path to a JSON file.
-  # };
 
   programs.bash.enable = true;
 
---
+++ modules/home-manager/utils/emacs/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+{
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-pgtk;
+    extraPackages = epkgs: [
+      epkgs.treesit-grammars.with-all-grammars
+
+      pkgs.jetbrains-mono
+      pkgs.nerd-fonts.jetbrains-mono
+      pkgs.nerd-fonts.symbols-only
+
+      # Rust
+      pkgs.rust-analyzer
+      pkgs.rustfmt
+      pkgs.cargo
+      pkgs.gcc_multi
+      # Python
+      pkgs.pyright
+      pkgs.ruff
+      # Nix
+      pkgs.nil
+      pkgs.nixd
+      pkgs.nixdoc
+      pkgs.nixfmt-classic
+      # Latex
+      pkgs.gnuplot
+      # Java
+      pkgs.jdt-language-server
+    ];
+  };
+}
